d17aabfdfb5da75c3221462db1cbc9ba6dada837
Annihilate <faria.danielx@gmail.com>
2026-09-10 17:06:51 -0300

    Fixed typo in activeRecords; added a Settings section in the Switch, only active if config is TRUE, acessed by typing -1 in menu, config on start removed; changed ignoreOveride to Overide, you can now Cancel the Input if Overide Check get's Triggered, Not Overiding will Complete the remaining free space, remade the Overide Warning, Removed the check for invalid amount; changed the way activeRecords are increased, now they increase every time the Input Loop Ends

--- src/main/java/br/dev/annihilate/cadastro_aluno/Cadastro_Aluno.java
+++ src/main/java/br/dev/annihilate/cadastro_aluno/Cadastro_Aluno.java
@@ -9,7 +9,7 @@ import java.util.Scanner;
  */
 public class Cadastro_Aluno {
 
-    public static int MAX = 2;
+    public static int MAX = 10;
 
     public static void main(String[] args) {
 
@@ -26,38 +26,20 @@ public class Cadastro_Aluno {
         int anoAtual = 2026;
         boolean invalidValue = true;
         boolean active = true;
-        boolean ignoreOveride = false;
+        boolean overide = false;
         int state = -1;
-        int ativeRecords = 0;
+        int activeRecords = 0;
 
 //        TEST CONFIGS
         boolean Config = true;
         int mode = 1;
-        System.out.println("\n------------------------------------------\nWelcome\n------------------------------------------\n");
 
-        if (Config == true) {
-            System.out.println("config mode on, use numbers to select options");
-            
-            
-            while (true) {
-                System.out.println("ID input: (1) = Random (2) = Manual");
-                int input = tecladoNumber.nextInt();
-                
-                if (input == 1){
-                    System.out.println("Random Selected");
-                    mode = 1;
-                    break;
-                } else if(input==2){
-                    System.out.println("Manual Selected");
-                    mode = 2;
-                    break;
-                }
-            }
-        }
+//        INPUT/MENU
+        System.out.println("\n------------------------------------------\nWelcome\n------------------------------------------\n");
 
         while (active) {
 
-            System.out.println("Menu Options:");
+            System.out.println("\n\nMenu Options:");
             System.out.println("1) Cadastro   2) Listar");
             System.out.println("0) Sair");
 
@@ -67,83 +49,94 @@ public class Cadastro_Aluno {
             }
             switch (state) {
                 case 1:
-                    if (ativeRecords >= MAX) {
-                        while (true || ignoreOveride == false) {
-                            System.out.printf("\n----\nWaring, max amount reached, do you wish to start overiding?\n---\nBy selecting Y, You will start Overiding the Registers beggining on Register 0\n(Y) (N)");
-                            String tempValue = tecladoString.nextLine();
-                            if (tempValue == "Y") {
-                                ativeRecords = 0;
-                                break;
-                            } else if (tempValue == "N") {
-                                ignoreOveride = true;
-                                break;
-                            }
-                        }
-                    }
 
                     System.out.println("Quantos Alunos serao cadastrados?");
                     qtdAluno = tecladoNumber.nextInt();
 
-                    if (qtdAluno > MAX || qtdAluno < 1 || ativeRecords + qtdAluno > MAX) {
-                        System.out.printf("\n---\nInvalid Amount, try again. Avaliable registers: %d\n---\n", (MAX - ativeRecords));
-                        break;
-                    } else {
-                        System.out.printf("Cadastro de %d Aluno(s), Porfavor preencha as Informacoes requisitadas\n", qtdAluno);
-                        for (int i = ativeRecords; i < qtdAluno; i++) {
+                    if (activeRecords >= MAX || activeRecords + qtdAluno >= MAX) {
+                        while (true) {
+                            System.out.printf("\n----\nWaring, max amount reached, do you wish to start overiding?\n---\n"
+                                    + "By selecting 1, You will start Overiding the Registers beggining on Register 0\n"
+                                    + "By selecting 2 You will Stop at the MAX value / Exit Registration.\n"
+                                    + "By selecting 0 You will Cancel this Registration.\n"
+                                    + "Empty Records: %d"
+                                    + "\n======\n"
+                                    + "(1)YES \n"
+                                    + "(2)NO\n", (MAX - activeRecords));
+                            int tempValue = tecladoNumber.nextInt();
+                            if (tempValue == 1) {
+                                overide = true;
+                                break;
+                            } else if (tempValue == 2) {
+                                overide = false;
+                                qtdAluno += MAX - qtdAluno;
+                                break;
 
-                            if (mode == 1) {
-                                id[i] = randomizer.nextInt(1, 999999999);
-                            } else if (mode == 2) {
-                                System.out.printf("0 for Exit\nID: ");
-                                id[i] = tecladoNumber.nextInt();
-                                if (id[i]==0){break;}
+                            } else if (tempValue == 0) {
+                                qtdAluno = 0;
+                                break;
                             }
-                            System.out.printf("Nome: ");
-                            nome[i] = tecladoString.nextLine();
-
-                            System.out.printf("Telefone: ");
-                            telefone[i] = tecladoNumber.nextLong();
-                            while (invalidValue) {
-                                System.out.printf("Data de Nascimento, DIA: ");
-                                dia_nascimento[i] = tecladoNumber.nextInt();
-                                if (dia_nascimento[i] > 0 && dia_nascimento[i] < 32) {
-
-                                    invalidValue = false;
-                                } else {
-                                    System.out.println("Invalid day");
-                                }
-                            }
-                            invalidValue = true;
-                            while (invalidValue) {
-                                System.out.printf("Data de Nascimento, MES: ");
-                                mes_nascimento[i] = tecladoNumber.nextInt();
-                                if (mes_nascimento[i] > 0 && mes_nascimento[i] < 13) {
-
-                                    invalidValue = false;
-                                } else {
-                                    System.out.println("Invalid Month");
-
-                                }
-                            }
-                            invalidValue = true;
-                            while (invalidValue) {
-                                System.out.printf("Data de Nascimento, ANO: ");
-                                ano_nascimento[i] = tecladoNumber.nextInt();
-                                if (ano_nascimento[i] <= anoAtual && ano_nascimento[i] > 0) {
-                                    invalidValue = false;
-
-                                } else {
-                                    System.out.println("Invalid Year");
-
-                                }
-                            }
-                            System.out.printf("\noutput %d %02d %s %d %02d %02d %04d %02d \n", i, id[i], nome[i], telefone[i], dia_nascimento[i], mes_nascimento[i], ano_nascimento[i], anoAtual - ano_nascimento[i]);
                         }
-                        ativeRecords = +qtdAluno;
                     }
-                    break;
-//        output
-//                                         10           20                     12             3    3   4      5
+                    if (overide == false && activeRecords == MAX) {
+                        break;
+                    }
+                    System.out.printf("Cadastro de %d Aluno(s), Porfavor preencha as Informacoes requisitadas\n", qtdAluno);
+                    if (overide == true && activeRecords == 10) {
+                        activeRecords = 0;
+                    }
+                    for (int i = activeRecords; i < qtdAluno; activeRecords++) {
+
+                        if (mode == 1) {
+                            id[i] = randomizer.nextInt(1, 999999999);
+                        } else if (mode == 2) {
+                            System.out.printf("0 for Exit\nID: ");
+                            id[i] = tecladoNumber.nextInt();
+                            if (id[i] == 0) {
+                                break;
+                            }
+                        }
+                        System.out.printf("Nome: ");
+                        nome[i] = tecladoString.nextLine();
+
+                        System.out.printf("Telefone: ");
+                        telefone[i] = tecladoNumber.nextLong();
+                        while (invalidValue) {
+                            System.out.printf("Data de Nascimento, DIA: ");
+                            dia_nascimento[i] = tecladoNumber.nextInt();
+                            if (dia_nascimento[i] > 0 && dia_nascimento[i] < 32) {
+
+                                invalidValue = false;
+                            } else {
+                                System.out.println("Invalid day");
+                            }
+                        }
+                        invalidValue = true;
+                        while (invalidValue) {
+                            System.out.printf("Data de Nascimento, MES: ");
+                            mes_nascimento[i] = tecladoNumber.nextInt();
+                            if (mes_nascimento[i] > 0 && mes_nascimento[i] < 13) {
+
+                                invalidValue = false;
+                            } else {
+                                System.out.println("Invalid Month");
+
+                            }
+                        }
+                        invalidValue = true;
+                        while (invalidValue) {
+                            System.out.printf("Data de Nascimento, ANO: ");
+                            ano_nascimento[i] = tecladoNumber.nextInt();
+                            if (ano_nascimento[i] <= anoAtual && ano_nascimento[i] > 0) {
+                                invalidValue = false;
+
+                            } else {
+                                System.out.println("Invalid Year");
+
+                            }
+                        }
+                        System.out.printf("\noutput %d %02d %s %d %02d %02d %04d %02d \n", i, id[i], nome[i], telefone[i], dia_nascimento[i], mes_nascimento[i], ano_nascimento[i], anoAtual - ano_nascimento[i]);
+                    }
                 case 2:
 
                     if (id[0] == 0) {
@@ -163,12 +156,37 @@ public class Cadastro_Aluno {
 
                     }
                     break;
+
                 case 0:
                     active = false;
                     System.out.println("GoodBye :3");
                     break;
+
+                case -1:
+                    if (Config == true) {
+                        System.out.println("\n=========\nconfig mode on, use numbers to select options\n==========\n");
+
+                        while (true) {
+                            System.out.println("ID input: (1) = Random (2) = Manual");
+                            int input = tecladoNumber.nextInt();
+
+                            if (input == 1) {
+                                System.out.println("\n=========\nRandom Selected\n=========\n");
+                                mode = 1;
+                                break;
+                            } else if (input == 2) {
+                                System.out.println("\n=========\nManual Selected\n=========\n");
+                                mode = 2;
+                                break;
+                            }
+                        }
+
+                    }
+                    System.out.printf("\n=====\nMAX VALUE\n=====\n default == 10\n-----\n");
+                    MAX = tecladoNumber.nextInt();
+                    break;
+
             }
         }
     }
 }
-    
