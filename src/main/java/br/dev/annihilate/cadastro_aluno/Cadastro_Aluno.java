da8632ff8fffac5d1a008378188998ce1d25be60
Annihilate <faria.danielx@gmail.com>
2026-09-10 11:29:50 -0300

    added Menu and some checks for invalid values

--- src/main/java/br/dev/annihilate/cadastro_aluno/Cadastro_Aluno.java
+++ src/main/java/br/dev/annihilate/cadastro_aluno/Cadastro_Aluno.java
@@ -9,74 +9,111 @@ import java.util.Scanner;
  */
 public class Cadastro_Aluno {
 
+    public static int MAX = 10;
+
     public static void main(String[] args) {
 
         Random randomizer = new Random();
         Scanner tecladoNumber = new Scanner(System.in);
         Scanner tecladoString = new Scanner(System.in);
-        int[] id = new int[1];
-        String[] nome = new String[10];
-        long[] telefone = new long[10];
-        int[] dia_nascimento = new int[10];
-        int[] mes_nascimento = new int[10];
-        int[] ano_nascimento = new int[10];
+        int[] id = new int[MAX];
+        int qtdAluno= 0;
+        String[] nome = new String[MAX];
+        long[] telefone = new long[MAX];
+        int[] dia_nascimento = new int[MAX];
+        int[] mes_nascimento = new int[MAX];
+        int[] ano_nascimento = new int[MAX];
         int anoAtual = 2026;
+        boolean invalidValue = true;
         boolean active = true;
+        int state = -1;
 
-        for (int i = 0; i < id.length; i++) {
+        System.out.println("\n-------\nWelcome\n-------\n");
 
-            id[i] = randomizer.nextInt(1,999999999);
+        while (active) {
 
-            System.out.printf("Nome: ");
-            nome[i] = tecladoString.nextLine();
-
-            System.out.printf("Telefone: ");
-            telefone[i] = tecladoNumber.nextLong();
-            while (active) {
-                System.out.printf("Data de Nascimento, DIA: ");
-                dia_nascimento[i] = tecladoNumber.nextInt();
-                if (dia_nascimento[i] > 0 && dia_nascimento[i] < 32) {
-
-                    active = false;
-                } else {
-                    System.out.println("Invalid day");
-                }
+            System.out.println("Menu Options:");
+            System.out.println("1) Cadastro   2) Listar");
+            System.out.println("0) Sair");
+            
+            state = tecladoNumber.nextInt();
+            if (state > 2 || state <0) {
+            System.out.println("\ninvalid State.");
             }
-            active = true;
-            while (active) {
-                System.out.printf("Data de Nascimento, MES: ");
-                mes_nascimento[i] = tecladoNumber.nextInt();
-                if (mes_nascimento[i] > 0 && mes_nascimento[i] < 13) {
+            switch (state) {
 
-                    active = false;
-                } else {
-                    System.out.println("Invalid Month");
+                case 1:
+                    System.out.println("Quantos Alunos serao cadastrados?");
+                    qtdAluno = tecladoNumber.nextInt();
+                    
+                    if (qtdAluno > MAX || qtdAluno <1){
+                        System.out.println("\n---\nInvalid Amount, try again.\nMax is 10, min is 1\n---\n");
+                        break;
+                    }
+                    
+                    System.out.printf("Cadastro de %d Aluno(s), Porfavor preencha as Informacoes requisitadas\n",qtdAluno);
+                    for (int i = 0; i < qtdAluno; i++) {
 
-                }
-            }
-            active = true;
-            while (active) {
-                System.out.printf("Data de Nascimento, ANO: ");
-                ano_nascimento[i] = tecladoNumber.nextInt();
-                if (ano_nascimento[i] <= anoAtual && ano_nascimento[i] > 0) {
-                    active = false;
+                        id[i] = randomizer.nextInt(1, 999999999);
 
-                } else {
-                    System.out.println("Invalid Year");
+                        System.out.printf("Nome: ");
+                        nome[i] = tecladoString.nextLine();
 
-                }
-            }
-            System.out.printf("\noutput %d %02d %s %d %02d %02d %04d %02d \n", i, id[i], nome[i], telefone[i], dia_nascimento[i], mes_nascimento[i], ano_nascimento[i], anoAtual - ano_nascimento[i]);
-        }
+                        System.out.printf("Telefone: ");
+                        telefone[i] = tecladoNumber.nextLong();
+                        while (invalidValue) {
+                            System.out.printf("Data de Nascimento, DIA: ");
+                            dia_nascimento[i] = tecladoNumber.nextInt();
+                            if (dia_nascimento[i] > 0 && dia_nascimento[i] < 32) {
 
+                                invalidValue = false;
+                            } else {
+                                System.out.println("Invalid day");
+                            }
+                        }
+                        invalidValue = true;
+                        while (invalidValue) {
+                            System.out.printf("Data de Nascimento, MES: ");
+                            mes_nascimento[i] = tecladoNumber.nextInt();
+                            if (mes_nascimento[i] > 0 && mes_nascimento[i] < 13) {
+
+                                invalidValue = false;
+                            } else {
+                                System.out.println("Invalid Month");
+
+                            }
+                        }
+                        invalidValue = true;
+                        while (invalidValue) {
+                            System.out.printf("Data de Nascimento, ANO: ");
+                            ano_nascimento[i] = tecladoNumber.nextInt();
+                            if (ano_nascimento[i] <= anoAtual && ano_nascimento[i] > 0) {
+                                invalidValue = false;
+
+                            } else {
+                                System.out.println("Invalid Year");
+
+                            }
+                        }
+                        System.out.printf("\noutput %d %02d %s %d %02d %02d %04d %02d \n", i, id[i], nome[i], telefone[i], dia_nascimento[i], mes_nascimento[i], ano_nascimento[i], anoAtual - ano_nascimento[i]);
+                    }
+                    break;
 //        output
-//                            10           20                     12             3    3   4      5
-        System.out.println("|------------|----------------------|--------------|-------------|-------|");
-        System.out.println("| ID         | NOME                 | Telefone     | dia/mes/ano | idade |");
-        System.out.println("|------------|----------------------|--------------|-------------|-------|");
-        for (int i = 0; i < id.length; i++) {
-            System.out.printf("| %10d | %20s | %12d |  %02d/%02d/%04d | %5d |\n", id[i], nome[i], telefone[i], dia_nascimento[i], mes_nascimento[i], ano_nascimento[i], anoAtual - ano_nascimento[i]);
-            System.out.println("|------------|----------------------|--------------|-------------|-------|");
+//                                         10           20                     12             3    3   4      5
+                case 2:
+                    System.out.println("|------------|----------------------|--------------|-------------|-------|");
+                    System.out.println("| ID         | NOME                 | Telefone     | dia/mes/ano | idade |");
+                    System.out.println("|------------|----------------------|--------------|-------------|-------|");
+                    for (int i = 0; i < id.length; i++) {
+                        System.out.printf("| %10d | %20s | %12d |  %02d/%02d/%04d | %5d |\n", id[i], nome[i], telefone[i], dia_nascimento[i], mes_nascimento[i], ano_nascimento[i], anoAtual - ano_nascimento[i]);
+                        System.out.println("|------------|----------------------|--------------|-------------|-------|");
+                    }
+                    break;
+                case 0:
+                    active = false;
+                    System.out.println("GoodBye :3");
+                    break;
+            }
         }
     }
 }
